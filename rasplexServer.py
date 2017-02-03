0c4701c9432b92aeef964be1fec2f57fd8c3413a
Nick Monteleone <nicholas.monteleone@uconn.edu>
2017-02-03 13:40:39 -0500

    redwrote the server using a simple TCP connection in python because the simple HTTP Server would not work on rasplex

--- rasplexServer.py
+++ rasplexServer.py
@@ -1,80 +1,75 @@
-from HTTPServer import BaseHTTPRequestHandler, HTTPServer
+from socket import *
 import subprocess
 
+serverPort = 12000
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
 
-class HTTPServerHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
-		message = "INVALID REQUEST\n"
-		pathArr = self.path.split("?")
-		self.path = pathArr[0]
-		varString = pathArr[1]
-		if self.path == "/":
-			message = "TRUE\n"
-		elif self.path == "/currentOS":
-			message = "rasplex\n"
-		self.wfile.write(bytes(message))
-		return
-
-	def do_POST(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
+def getVars(varString):
+	varArr = varString.split("&")
+	variables = {}
+	for v in varArr:
+		try:
+			splitted = v.split("=")
+			variable = splitted[0]
+			value = splitted[1]
+			variables[variable] = value
+		except:
+			continue
+	return variables
+print 'The server is ready to receive'
+while 1:
+	try:
+		connectionSocket, addr = serverSocket.accept()
+		request = connectionSocket.recv(1024)
+		HTTPHeaders = request.split("\n")
+		requestHeader = HTTPHeaders[0].split(" ")
+		method = requestHeader[0]
+		path = requestHeader[1]
 		message = "INVALID REQUEST"
-		if self.path == "/switchOS":
-			varibles = self.getVars()
-			if "osName" in varibles.keys():
-				newOS = varibles["osName"].lower()
-				if newOS == 'raspbian' or newOS == 'retropie' or newOS == "kodi":
-					try:
-						message = subprocess.check_output(newOS, shell=True).decode()
-					except Exception as e:
-						message = "Switch was not succesffully excecuted: %s" % str(e)
+		if method == "GET":
+			pathStr = path
+			if "?" in pathStr:
+				pathArr = pathStr.split("?")
+				path = pathArr[0]
+				variableStr = pathArr[1]
+				variables = getVars(variableStr)
+			if path == "/":
+				message = "TRUE\n"
+			elif path == "/currentOS":
+				message = "rasplex\n"
+		elif method == "POST":
+			variableArr = HTTPHeaders[-1]
+			if path == "/switchOS":
+				variables = getVars(variableArr)
+				if "osName" in variables.keys():
+					newOS = variables["osName"].lower()
+					if newOS == "raspbian" or newOS == "retropie" or newOS == "kodi":
+						try:
+							message = subprocess.check_output(newOS, shell=True).decode()
+						except Exception as e:
+							message = "Switch was not sucessfully excecuted: %s" % str(e)
+					else:
+						message = "Invalid OS"
 				else:
-					message = "Invalid OS"
-			else:
-				message = "Please enter an OS to switch to"
-		elif self.path == "/reboot":
-			try:
-				message = subprocess.check_output("reboot", shell=True).decode()
-			except:
-				message = "reboot failed"
-		elif self.path == "/rca":
-			try:
-				message = subprocess.check_output("rca", shell=True).decode()
-			except:
-				message = "reboot failed"
-		elif self.path == "/hdmi":
-			try:
-				message = subprocess.check_output("hdmi", shell=True).decode()
-			except:
-				message = "reboot failed"
-		self.wfile.write(bytes(message))
-
-	def getVars(self):
-		varString = self.rfile.read(int(self.headers['Content-Length'])).decode()
-		varArray = varString.split("&")
-		varDict = {}
-		for v in varArray:
-			name = v.split("=")[0]
-			value = v.split("=")[1]
-			varDict[name] = value
-		return varDict
-
-
-
-def run():
-	portNo = 9876
-	print("starting server at localhost:%d" % portNo)
-
-	#checkServer
-	server_address = ("localhost", 9876)
-	httpdCheckServer = HTTPServer(server_address, HTTPServerHandler)
-	httpdCheckServer.serve_forever()
-
-
-	print("running server...")
-
-run()
+					message = "Please enter an OS to switch to"
+			elif path == "/reboot":
+				try:
+					message = subprocess.check_output("reboot", shell=True).decode()
+				except:
+					message = "reboot failed:"
+			elif path == "/rca":
+				try:
+					message = subprocess.check_output("rca", shell=True).decode()
+				except:
+					message = "reboot failed"
+			elif path == "/hdmi":
+				try:
+					message = subprocess.check_output("hdmi", shell=True).decode()
+				except:
+					message = "reboot failed"
+		connectionSocket.send(message)
+		connectionSocket.close()
+	finally:
+		connectionSocket.close()
