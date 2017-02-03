94ace8413ba4868988223b4b54aeecd771b0a3cf
Nick Monteleone <nicholas.monteleone@uconn.edu>
2017-02-02 19:58:07 -0500

    wrote up the HTTP Server commands

--- rasplexServer.py
+++ rasplexServer.py
@@ -1,22 +1,77 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
+import subprocess
 
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+
+class HTTPServerHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		self.send_response(200)
 		self.send_header("Content-type", "text/html")
 		self.end_headers()
-		os.system("python test.py")
-		message = "Hello World!\n"
+		message = "INVALID REQUEST"
+		if self.path == "/":
+			message = "TRUE\n"
+		elif self.path == "/currentOS":
+			message = "rasplex\n"
 		self.wfile.write(bytes(message, 'utf8'))
 		return
 
+	def do_POST(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+		message = "INVALID REQUEST"
+		if self.path == "/switchOS":
+			varibles = self.getVars()
+			if "osName" in varibles.keys():
+				newOS = varibles["osName"].lower()
+				if newOS == 'raspbian' or newOS == 'retropie' or newOS == "kodi":
+					try:
+						message = subprocess.check_output(newOS, shell=True).decode()
+					except Exception as e:
+						message = "Switch was not succesffully excecuted: %s" % str(e)
+				else:
+					message = "Invalid OS"
+			else:
+				message = "Please enter an OS to switch to"
+		elif self.path == "/reboot":
+			try:
+				message = subprocess.check_output("reboot", shell=True).decode()
+			except:
+				message = "reboot failed"
+		elif self.path == "/rca":
+			try:
+				message = subprocess.check_output("rca", shell=True).decode()
+			except:
+				message = "reboot failed"
+		elif self.path == "/hdmi":
+			try:
+				message = subprocess.check_output("hdmi", shell=True).decode()
+			except:
+				message = "reboot failed"
+		self.wfile.write(bytes(message, 'utf8'))
+
+	def getVars(self):
+		varString = self.rfile.read(int(self.headers['Content-Length'])).decode()
+		varArray = varString.split("&")
+		varDict = {}
+		for v in varArray:
+			name = v.split("=")[0]
+			value = v.split("=")[1]
+			varDict[name] = value
+		return varDict
+
+
+
 def run():
-	portNo = 6666
+	portNo = 9876
 	print("starting server at localhost:%d" % portNo)
-	server_address= ("localhost", 6666)
-	httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+
+	#checkServer
+	server_address = ("localhost", 9876)
+	httpdCheckServer = HTTPServer(server_address, HTTPServerHandler)
+	httpdCheckServer.serve_forever()
+
+
 	print("running server...")
-	httpd.serve_forever()
 
 run()
